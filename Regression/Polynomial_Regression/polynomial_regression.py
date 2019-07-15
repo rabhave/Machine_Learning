5b7ab3be38a197deab3d210eacfecb08eb5ab2be
Rajan Arun Bhave <40522832+rabhave@users.noreply.github.com>
2019-07-15 12:01:59 +0530

    dataset path issue solved

--- Regression/Polynomial_Regression/polynomial_regression.py
+++ Regression/Polynomial_Regression/polynomial_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv("Position_Salaries_dataset.csv",encoding='utf-8')
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
@@ -60,7 +60,6 @@ plt.ylabel('Salary')
 plt.show()
 
 # Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
-
+#lin_reg.predict(6.5)
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
+#lin_reg_2.predict(poly_reg.fit_transform(6.5))
